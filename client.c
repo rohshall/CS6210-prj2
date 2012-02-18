70e36b2d6b1a020182d489faf96f9df7e957531f
Sam Britt <samuelbritt@gmail.com>
2012-02-17 21:33:47 -0500

    rename connect(), formatting

--- client.c
+++ client.c
@@ -21,30 +21,26 @@ static void install_sig_handler(int signo, void (*handler)(int))
 	  exit(1);
 }
 
-void connect(struct fs_registrar *reg)
+static void register_with_server()
 {
-  sem_wait(&(reg->empty));
-  sem_wait(&(reg->mtx));
+	struct fs_registrar *reg = shm_map(shm_registrar_name, sizeof(*reg));
+	sem_wait(&(reg->empty));
+	sem_wait(&(reg->mtx));
 
-  reg->registrar[reg->client_index].client_pid = getpid();
-  reg->client_index++;
+	reg->registrar[reg->client_index].client_pid = getpid();
+	reg->client_index++;
 
-  sem_post(&(reg->mtx));
-  sem_post(&(reg->full));
+	sem_post(&(reg->mtx));
+	sem_post(&(reg->full));
 
-  while(!ready)
-    sleep(1);
+	while(!ready)
+		sleep(1);
 }
 
 int main(int argc, char const *argv[])
 {
-  
-  install_sig_handler(SIGUSR1, &sigusr_handler);
-
-  struct fs_registrar *reg = shm_map(shm_registrar_name, sizeof(*reg));
-
-
-  connect(reg);
+	install_sig_handler(SIGUSR1, &sigusr_handler);
+	register_with_server();
 
 	printf("hello client world\n");
 	return 0;
