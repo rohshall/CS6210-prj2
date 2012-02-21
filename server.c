2d0246744b7ef998ce583d793d6713ebc8509a18
Sam Britt <samuelbritt@gmail.com>
2012-02-20 22:44:44 -0500

    add linked list functions

--- server.c
+++ server.c
@@ -9,6 +9,50 @@
 /* set to 1 when we must exit */
 volatile sig_atomic_t done = 0;
 
+/* Node for the linked list of registered server threads */
+struct server_list_node {
+	struct server_list_node *next;
+	union fs_process_sring_entry *entry;
+	int has_work;
+	pthread_mutex_t mtx;	/* protect has_work */
+	pthread_cond_t cond;
+	/* The following two fields are for signaling the pthread to stop */
+	sem_t *sem;
+	pthread_t tid;
+};
+
+/* global linked list */
+struct server_list {
+	struct server_list_node *list;
+	sem_t full;
+	sem_t mtx;
+} server_list;
+
+void server_threads_list_init(struct server_list *list)
+{
+	list->list = NULL;
+	sem_init(&list->full, 0, 0);
+	sem_init(&list->mtx, 0, 1);
+}
+
+/* Alloc's, initializes and returns a new server_list_node */
+struct server_list_node *server_list_node_create()
+{
+	struct server_list_node *n = ecalloc(sizeof(*n));
+	pthread_mutex_init(&n->mtx, NULL);
+	pthread_cond_init(&n->cond, NULL);
+	return n;
+}
+
+/* Destroy's a server_list_node */
+void server_list_node_destroy(struct server_list_node *n)
+{
+	pthread_mutex_destroy(&n->mtx);
+	pthread_cond_destroy(&n->cond);
+	free(n);
+}
+
+/* Sig handler for exit signal */
 static void exit_handler(int signo)
 {
 	done = 1;
@@ -105,6 +149,10 @@ static void reg_handle_request(union fs_registrar_sring_entry *entry)
 /* starts the infinite loop for the client registrar */
 static void start_registrar()
 {
+	/* Create the internal circular linked list for worker threads */
+	server_threads_list_init(&server_list);
+
+	/* Create the registration ring buffer for clients */
 	struct fs_registrar_sring *reg = shm_create(shm_registrar_name,
 						    sizeof(*reg));
 	RB_INIT(fs_registrar, reg, FS_REGISTRAR_SLOT_COUNT);
